420b8c8a1d42c8219e58ccc8402ef66cb403cd56
Mischa Daamen <msvdaamen@outlook.com>
2024-08-23 18:19:40 +0200

    Remove log

--- database/.config/kysely.config.ts
+++ database/.config/kysely.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'kysely-ctl'
 import {PostgresJSDialect} from 'kysely-postgres-js'
 import postgres from 'postgres'
 
-console.log(process.env.DATABASE_URL);
-
 export default defineConfig({
 	// replace me with a real dialect instance OR a dialect name + `dialectConfig` prop.
 	dialect: new PostgresJSDialect({
 		postgres: postgres(process.env.DATABASE_URL),
+
 	}),
 	migrations: {
 		migrationTableName: 'migrations'
