9436f686a553540cdcd561fac1bafdaa2b00d72f
Mischa Daamen <mischa.daamen@print.com>
2024-12-29 11:31:42 +0100

    add store

--- apps/food-swipe-admin/src/app/app.config.ts
+++ apps/food-swipe-admin/src/app/app.config.ts
@@ -9,6 +9,7 @@ import { AuthInterceptor } from './modules/auth/interceptors/auth.interceptor';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { DEFAULT_DIALOG_CONFIG } from '@angular/cdk/dialog';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,13 +18,14 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
     {
-      provide: DEFAULT_DIALOG_CONFIG,
-      useValue: {
-        hasBackdrop: true,
-        width: '40vw',
-        minWidth: '300px',
-        panelClass: 'default-dialog',
-      },
+        provide: DEFAULT_DIALOG_CONFIG,
+        useValue: {
+            hasBackdrop: true,
+            width: '40vw',
+            minWidth: '300px',
+            panelClass: 'default-dialog',
+        },
     },
-  ],
+    provideStore()
+],
 };
