7ac9f910af916b2072ad3fba0f78114b5b25b7ff
Mischa Daamen <msv.daamen@outlook.com>
2025-03-11 18:55:59 +0100

    Better

--- apps/food-swipe-mobile/capacitor.config.ts
+++ apps/food-swipe-mobile/capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.foodswipe.app',
   appName: 'food-swipe',
   webDir: 'dist/food-swipe/browser',
-  server: {
-    url: 'http://192.168.2.49::4200',
-    cleartext: true,
-  },
 };
 
 export default config;
