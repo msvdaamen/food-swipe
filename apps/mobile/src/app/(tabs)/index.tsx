0223c69065504da6acd15772d8eb763eb286e59e
Mischa Daamen <mischa.daamen@print.com>
2025-04-03 23:27:04 +0200

    Basic tiles

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { AppText } from '@/components/ui/text';
-import { StyleSheet, View } from 'react-native';
+import { AppText } from "@/components/ui/text";
+import { StyleSheet, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-          <AppText style={styles.title}>Home</AppText>
+      <AppText style={styles.title}>Home</AppText>
     </View>
   );
 }
@@ -12,16 +12,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- apps/mobile/src/app/(tabs)/recipes.tsx
+++ apps/mobile/src/app/(tabs)/recipes.tsx
@@ -1,31 +1,49 @@
-import { AppText } from '@/components/ui/text';
-import { AppInput } from '@/components/ui/input';
-import { StyleSheet, View } from 'react-native';
-import { Clock, Search } from 'lucide-react-native';
-import { Image } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { AppText } from "@/components/ui/text";
+import { AppInput } from "@/components/ui/input";
+import { StyleSheet, View } from "react-native";
+import { Clock, Search } from "lucide-react-native";
+import { Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <View>
-      <AppInput placeholder="Search recipes" Icon={Search} />
+        <AppInput placeholder="Search recipes" Icon={Search} />
       </View>
-      <AppText style={styles.title}>Recipes</AppText>
       <View style={styles.recipesContainer}>
         <View style={styles.recipeCard}>
-          <Image style={styles.recipeImage} source={{uri: 'https://static.food-swipe.app/9b9b6ccc-ac28-45af-a245-1d0dd9d00547.jpeg'}}/>
+          <Image
+            style={styles.recipeImage}
+            source={{
+              uri: "https://static.food-swipe.app/9b9b6ccc-ac28-45af-a245-1d0dd9d00547.jpeg",
+            }}
+          />
           <View style={styles.recipeCardOverlay}>
-          <LinearGradient
-        // Background Linear Gradient
-        colors={['transparent', 'rgba(0, 0, 0, 0.5)']}
-        style={{width: '100%', height: '100%', position: 'absolute', inset: 0}}
-      />
+            <LinearGradient
+              colors={["transparent", "rgba(0, 0, 0, 0.5)"]}
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                inset: 0,
+              }}
+            />
             <View style={styles.recipeCardOverlayContent}>
-              <AppText style={styles.recipeCardText}>Recipe Name</AppText>
+              <AppText style={styles.recipeCardText}>
+                Airfryer chicken 'tandoori' with rice and cucumber skyr
+              </AppText>
               <View style={styles.recipeCardDetailsContainer}>
                 <AppText style={styles.recipeCardDetailText}>1 cal</AppText>
-                <View style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'row', gap: 4}}>
+                <View
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row",
+                    gap: 4,
+                  }}
+                >
                   <Clock size={16} color="#d1d5db" />
                   <AppText style={styles.recipeCardDetailText}>25 mins</AppText>
                 </View>
@@ -40,61 +58,62 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    paddingHorizontal: 16
+    display: "flex",
+    paddingHorizontal: 16,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   recipesContainer: {
-    display: 'flex',
+    display: "flex",
     marginBottom: 4,
-    gap: 16
+    gap: 16,
+    marginTop: 16,
   },
   recipeCard: {
-    position: 'relative',
-    overflow: 'hidden'
+    position: "relative",
+    overflow: "hidden",
   },
   recipeCardOverlay: {
-    position: 'absolute',
+    position: "absolute",
     inset: 0,
     // backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    zIndex: 100
+    zIndex: 100,
   },
   recipeCardOverlayContent: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
-    width: '100%',
+    width: "100%",
     padding: 12,
-    color: 'white',
-    zIndex: 100
+    color: "white",
+    zIndex: 100,
   },
   recipeCardText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   recipeCardDetailsContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   recipeCardDetailText: {
-    color: '#d1d5db',
+    color: "#d1d5db",
     fontSize: 12,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   recipeImage: {
-    width: '100%',
+    width: "100%",
     height: 200,
-    objectFit: 'cover',
-    borderRadius: 4
-  }
+    objectFit: "cover",
+    borderRadius: 4,
+  },
 });
