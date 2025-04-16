5661e42aa1a7e466d9814711fb58ada02f1b4c71
Mischa Daamen <mischa.daamen@print.com>
2025-04-16 20:44:49 +0200

    ch

--- apps/mobile/src/app/(logged-in)/(tabs)/recipe-books.tsx
+++ apps/mobile/src/app/(logged-in)/(tabs)/recipe-books.tsx
@@ -1,8 +1,8 @@
 import { AppText } from "@/components/ui/text";
-import { ChevronRight, Heart } from "lucide-react-native";
-import { StyleSheet, useColorScheme, View, FlatList, ScrollView } from "react-native";
+import { BookOpen, ChevronRight, Heart } from "lucide-react-native";
+import { StyleSheet, useColorScheme, View, FlatList, ScrollView, ViewStyle, StyleProp, RefreshControl } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-
+import { useState } from "react";
 export default function RecipeBooksScreen() {
   const theme = useColorScheme();
 
@@ -10,8 +10,11 @@ export default function RecipeBooksScreen() {
   const likedBackgroundColor = theme === "dark" ? "#424242" : "#ffffff";
   const likedTextColor = theme === "dark" ? "#9ca3af" : "#6b7280";
 
-  const data = new Array(199).fill(0);
+  const data = new Array(41).fill(0);
   const gap = 10;
+  const grid = toGrid(data, 2);
+
+  const [refreshing, setRefreshing] = useState(false);
   return (
     <View style={styles.container}>
       <View
@@ -48,30 +51,48 @@ export default function RecipeBooksScreen() {
       <View style={styles.yourBooksHeader}>
         <AppText style={styles.yourBooksHeaderText}>Your Books</AppText>
       </View>
+      {/* <FlatList >
+        {grid.map((row, rowIndex) => (
+          <View key={rowIndex} style={styles.yourBooksRow}>
+            {row.map((item, itemIndex) => (
+              <RecipeBookItem key={itemIndex} style={{flex: 1/2, marginRight: (itemIndex === 0) ? 5 : 0, marginLeft: (itemIndex === 1) ? 5 : 0}} />
+            ))}
+          </View>
+        ))}
+      </FlatList> */}
       <FlatList 
         data={data}
-        renderItem={({ item }) => <RecipeBookItem />}
-        contentContainerStyle={{gap}}
-        columnWrapperStyle={{gap}}
+        style={{flex: 1}}
+        renderItem={({ item, index }) => <RecipeBookItem even={index % 2 === 0} last={index === data.length - 1} />}
         numColumns={2}
         keyExtractor={(item, index) => index.toString()}
+        columnWrapperStyle={{marginBottom: 10}}
+        // refreshControl={<RefreshControl refreshing={true} />}
       />
         
     </View>
   );
 }
 
-function RecipeBookItem() {
+function RecipeBookItem({even, last}: { even: boolean, last: boolean}) {
   const theme = useColorScheme();
 
   const backgroundColor = theme === "dark" ? "#424242" : "#ffffff";
   const color = theme === "dark" ? "#9ca3af" : "#6b7280";
+
+  let margin = 10;
+  if (last) {
+    margin = margin * 2;
+  }
+  const marginRight = even ? margin : 0;
+  const marginLeft = even ? 0 : margin;
 
   return (
     <View
     style={[
       styles.yourBooksItem,
       { backgroundColor },
+      { marginRight, marginLeft }
     ]}
   >
     <LinearGradient
@@ -79,7 +100,9 @@ function RecipeBookItem() {
       colors={["#f472b6", "#ef4444"]}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
-    ></LinearGradient>
+    >
+     <BookOpen size={45} color="white" />
+    </LinearGradient>
     <View style={styles.yourBooksItemTextContainer}>
       <AppText>Your asdf</AppText>
       <AppText style={{ color }}>12 recipes</AppText>
@@ -88,9 +111,21 @@ function RecipeBookItem() {
   )
 }
 
+function toGrid<T>(items: T[], columns: number): T[][] {
+  return items.reduce((acc, item, index) => {
+    const columnIndex = Math.floor(index / columns);
+    if (!acc[columnIndex]) {
+      acc[columnIndex] = [];
+    }
+    acc[columnIndex].push(item);
+    return acc;
+  }, [] as T[][]);
+} 
+
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 16,
+    flex: 1
   },
   likedContainer: {
     borderRadius: 12,
@@ -135,14 +170,22 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: 'flex-start'
   },
+  yourBooksRow: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 10
+  },
   yourBooksItem: {
     borderRadius: 12,
     overflow: "hidden",
-    flex: 1/2,
+    flex: 1/2
   },
   yourBooksItemGradient: {
     height: 128,
     width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   yourBooksItemTextContainer: {
     padding: 12,
